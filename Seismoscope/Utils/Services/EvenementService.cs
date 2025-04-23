b18fc77f71a3dba11e4f51855c90d4cdf5b5b36c
Ahmed-Ezzi <ezziahmed118@gmail.com>
2025-04-22 20:35:31 -0400

    ajout service evenement

---
+++ Seismoscope/Utils/Services/EvenementService.cs
@@ -0,0 +1,31 @@
+﻿using Seismoscope.Model.Interfaces;
+using Seismoscope.Model;
+using Seismoscope.Utils.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seismoscope.Utils.Services
+{
+    public class EvenementService : IEvenementService
+    {
+        private readonly IEvenementDAL _evenementDal;
+
+        public EvenementService(IEvenementDAL evenementDal)
+        {
+            _evenementDal = evenementDal;
+        }
+
+        public List<EvenementSismique> ObtenirParStation(int stationId)
+        {
+            return _evenementDal.ObtenirEvenementsParStation(stationId);
+        }
+
+        public void AjouterEvenement(EvenementSismique evenement)
+        {
+            _evenementDal.AjouterEvenement(evenement);
+        }
+    }
+}
