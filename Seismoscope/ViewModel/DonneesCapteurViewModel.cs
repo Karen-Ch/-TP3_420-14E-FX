7c1672d6cb651fbeecbf233cd5508d15d780a09a
Ahmed-Ezzi <ezziahmed118@gmail.com>
2025-04-29 14:30:22 -0400

    ajout methodes détection d'événements sismiques

--- Seismoscope/ViewModel/DonneesCapteurViewModel.cs
+++ Seismoscope/ViewModel/DonneesCapteurViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Win32;
+using Seismoscope.Enums;
 using Seismoscope.Model;
 using Seismoscope.Model.Interfaces;
 using Seismoscope.Utils;
 using Seismoscope.Utils.Commands;
+using Seismoscope.Utils.Services;
 using Seismoscope.Utils.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -23,6 +25,8 @@ namespace Seismoscope.ViewModel
         private readonly ICapteurService _capteurService;
         private readonly IStationService _stationService;
         private readonly IDialogService _dialogService;
+        private readonly IEvenementService _evenementService;
+
 
 
         public ObservableCollection<Capteur> Capteurs { get; set; }
@@ -45,13 +49,16 @@ namespace Seismoscope.ViewModel
             INavigationService navigationService,
             ICapteurService capteurService,
             IStationService stationService,
-            IDialogService dialogService)
+            IDialogService dialogService,
+            IEvenementService evenementService)
         {
             _userSession = userSession;
             _navigationService = navigationService;
             _capteurService = capteurService;
             _stationService = stationService;
             _dialogService = dialogService;
+            _evenementService= evenementService;    
+
 
             Capteurs = new ObservableCollection<Capteur>(_capteurService.ObtenirTous());
             NouveauStationId = (int)_userSession.ConnectedUser.StationId;
@@ -124,8 +131,34 @@ namespace Seismoscope.ViewModel
                 OnPropertyChanged(nameof(Type));
                 OnPropertyChanged(nameof(Amplitude));
 
+                DetecterEvenement();
                 await Task.Delay(temps); 
             }
+        }
+        private void DetecterEvenement()
+        {
+            if (CapteurSelectionne != null && Amplitude > CapteurSelectionne.SeuilAlerte)
+            {
+                CreerEtEnregistrerEvenement();
+            }
+        }
+
+        private void CreerEtEnregistrerEvenement()
+        {
+            _dialogService.ShowMessage(
+                "Un événement sismique a été détecté (seuil dépassé) !",
+                "Alerte");
+
+            var evenement = new EvenementSismique
+            {
+                DateEvenement = DateTime.Now,
+                TypeOnde = (TypeOnde)Enum.Parse(typeof(TypeOnde), Type, true), 
+                Amplitude = Amplitude,
+                SeuilAtteint = CapteurSelectionne.SeuilAlerte,
+                StationId = CapteurSelectionne.StationId ?? 0
+            };
+
+            _evenementService.AjouterEvenement(evenement);
         }
     }
 }
