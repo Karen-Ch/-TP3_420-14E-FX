a866e98a348522775396bf1235fd9276327f1eab
Ahmed-Ezzi <ezziahmed118@gmail.com>
2025-05-12 19:34:28 -0400

    ajout journal service

---
+++ Seismoscope/Utils/Services/JournalService.cs
@@ -0,0 +1,29 @@
+﻿using Seismoscope.Model;
+using Seismoscope.Utils.Services.Interfaces.Seismoscope.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seismoscope.Utils.Services
+{
+    public class JournalService : IJournalService
+    {
+        public ObservableCollection<AjustementJournal> Journaux { get; } = new();
+
+        public void Enregistrer(int capteurId, string regle, string detail)
+        {
+            var entree = new AjustementJournal
+            {
+                Date = DateTime.Now,
+                CapteurId = capteurId,
+                RègleAppliquée = regle,
+                Détail = detail
+            };
+
+            Journaux.Add(entree);
+        }
+    }
+}
