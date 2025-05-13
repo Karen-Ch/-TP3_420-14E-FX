9a18e4dea27b778181f836d17b672f1f881a62b8
karencharret <kados001@edu.cegepgarneau.ca>
2025-05-13 00:36:57 -0400

    Ajoute de ConfigureService

---
+++ Seismoscope/Utils/Services/ConfigurationService.cs
@@ -0,0 +1,28 @@
+﻿using Seismoscope.Utils.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seismoscope.Utils.Services
+{
+    public class ConfigurationService : IConfigurationService
+    {
+        
+        public string GetDbPath()
+        {
+            return ConfigurationManager.AppSettings["DbPath"];
+        }
+        public string GetDefaultAdminUserName()
+        {
+            return ConfigurationManager.AppSettings["AdminUserName"];
+        }
+        public string GetDefaultAdminPassword()
+        {
+            return ConfigurationManager.AppSettings["AdminPassword"];
+        }   
+    }
+}
